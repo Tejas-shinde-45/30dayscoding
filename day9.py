95ddf5cee073698c333ff60fd7f6224a7b334d1c
Tejas-shinde-45 <i.tejasshinde.2004@gmail.com>
2026-03-30 14:34:10 +0530

    added new files

---
+++ day9.py
@@ -0,0 +1,4 @@
+# Day 9 – File Handling & Simple Tasks
+# Focus: File read/write + logic practice
+# Questions: 1. Count words in file 2. Count lines and characters 3. Find most frequent word 4. Write list data to file 5. Read numbers and print their sum
+# 🎯 Daily Challenge: Read a file and print top 3 most repeated words.
